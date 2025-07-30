c8aa5093b78297bda957bef770324245fa127bfe
paranoikcodit <paranoikpidoras@gmail.com>
2025-07-30 15:19:41 +0300

    Add support for bun-fetch field 'proxy'

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import {Client, cacheExchange, fetchExchange} from "@urql/core";
+import { Client, cacheExchange, fetchExchange } from "@urql/core";
 
-export const createClient = () =>
+export const createClient = (proxy?: string) =>
 	new Client({
 		fetchOptions: () => ({
+			proxy,
 			headers: {
 				"user-agent":
 					"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
